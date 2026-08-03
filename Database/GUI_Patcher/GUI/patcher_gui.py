6bbfdb52046df0c64cf6dc5bb9ae6dbf8886e666
anon <anon@local>
2026-08-03 15:41:03 +1000

    Fix Empyrea synthesis recipe

--- Database/GUI_Patcher/GUI/patcher_gui.py
+++ Database/GUI_Patcher/GUI/patcher_gui.py
@@ -28,7 +28,7 @@ def app_root():
     return Path(__file__).resolve().parents[3]
 
 ROOT = app_root()
-PATCHER_VERSION = "1.2.0"
+PATCHER_VERSION = "1.2.1"
 
 def open_url(url):
     if sys.platform.startswith("linux"):
